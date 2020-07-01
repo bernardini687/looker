f46a45cdd4565e6f406ed4e4810917e055ef4b69
oscar bernardini <bernardini687@gmail.com>
2020-07-01 20:19:06 +0200

    validate payload

--- src/body.ts
+++ src/body.ts
@@ -1,25 +1,40 @@
 import { ValidRecord } from './is_valid_record.ts'
 
 export class Body {
-  payload: ValidRecord
-  header: string
-  padding: number
-  leftMargin = '# '
-  separator = ':  '
+  readonly header: string
+  readonly leftMargin = '# '
+  readonly padding: number
+  readonly payload: ValidRecord
+  readonly separator = ':  '
 
   constructor(validRecord: ValidRecord) {
     const { header, ...payload } = validRecord
 
-    this.payload = payload
     this.header = header
+    this.payload = payload
+
+    this.validatePayload()
+
     this.padding = this.maxLabelSize()
+
+    // validateHeader() this.leftMargin.length + this.header.padStart(this.padding).length
   }
 
   maxLabelSize(): number {
+    // take into account the lenght of the header!
     return Math.max(...Object.keys(this.payload).map(x => x.length))
   }
 
-  // validPayload() {
-  //   this.payload
-  // }
+  validatePayload(): void {
+    const sizes = Object.entries(this.payload).map(
+      x => this.leftMargin.length + x[0].length + this.separator.length + x[1].length
+    )
+
+    console.log(sizes)
+    const maxSize = Math.max(...sizes)
+    // 88 should be a class constant
+    if (maxSize > 88) {
+      throw new Error(`get rid of ${maxSize - 88} characters`)
+    }
+  }
 }
--- src/body_test.ts
+++ src/body_test.ts
@@ -1,7 +1,7 @@
 import { assertEquals, assertThrows } from './dev_deps.ts'
 import { Body as Subject } from './body.ts'
 
-Deno.test('successfully build a Body with metadata', () => {
+Deno.test('successfully build a Body with header', () => {
   const validRecord = { header: 'foo', bar: 'baz' }
   const { header, ...payload } = validRecord
 
@@ -16,7 +16,7 @@ Deno.test('successfully build a Body with metadata', () => {
   })
 })
 
-Deno.test('successfully build a Body with metadata', () => {
+Deno.test('successfully build a Body without header', () => {
   const validRecord = { asdf: 'asdf', foo: 'bar' }
 
   const result = new Subject(validRecord)
@@ -28,4 +28,28 @@ Deno.test('successfully build a Body with metadata', () => {
     payload: validRecord,
     separator: ':  ',
   })
+})
+
+// Deno.test('throws if header exceeds max length', () => {
+//   const validRecord = { header: 'asdf'.repeat(20), asdf: 'asdf' }
+
+//   assertThrows(
+//     () => {
+//       new Subject(validRecord)
+//     },
+//     Error,
+//     'get rid of 1 character'
+//   )
+// })
+
+Deno.test('throw if payload exceeds max length', () => {
+  const validRecord = { foo: 'bar', asdf: 'asdf'.repeat(20) }
+
+  assertThrows(
+    () => {
+      new Subject(validRecord)
+    },
+    Error,
+    'get rid of 1 character'
+  )
 })
